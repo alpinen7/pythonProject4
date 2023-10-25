6fc4113484b69be0b16aebc79da6bd1f9c3fbc7e
Alexey Lebedev <lazdanny@mail.ru>
2023-10-26 01:51:16 +0300

    созданы impor_json и add_date в utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,26 @@
 import json
 
+
 def import_json(json_file):
     """возвращает json-строку из json-файла"""
-    with open("operations.json", "r") as file:
+    with open(json_file, "r") as file:
         return json.load(file)
 
 
-print(import_json("operations.json"))
+a = import_json("operations.json")
+print(a)
 
+
+def add_date(dictionary):
+    date_dict = []
+    ready_date_dict = []
+    for i in range(0, len(dictionary)):
+        for k in dictionary[i].items():
+            date_dict.append(dictionary[i]["date"])
+    for i in range(0, len(date_dict)):
+        ready_date_dict.append(date_dict[i][0:10])
+    # return ready_date_dict
+    return date_dict
+
+
+print(add_date(a))
